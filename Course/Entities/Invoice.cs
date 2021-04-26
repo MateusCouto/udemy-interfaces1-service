89696581cba8ac5ef6d12784096e21212dd25afa
Mateus Couto <42397879+MateusCouto@users.noreply.github.com>
2021-04-25 22:23:44 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Entities/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("Pagamento Básico: " + FormattedAmount(BasicPayment));
+            sb.AppendLine("Tax: " + FormattedAmount(Tax));
+            sb.AppendLine("Total Payament: " + FormattedAmount(TotalPayment));
+
+
+            return sb.ToString();
+        }
+        public string FormattedAmount( double amount)
+        {
+            CultureInfo.CurrentCulture = new("pt-BR", false);
+            return amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
